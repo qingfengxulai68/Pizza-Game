6fbd7f914e09a36d5ef97d51b39512db926bde47
qingfengxulai68 <166528560+qingfengxulai68@users.noreply.github.com>
2024-11-16 20:14:46 +0100

    Delete 4_Cuire directory

--- 4_Cuire/remise_a_zero.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Arrête tous les processus créés par le jeu
-pkill -f "Simulation"  
-pkill -f "Target_Pizza_Process"
-
-# Informe le joueur que l'environnement est réinitialisé
-echo "Tous les processus du jeu ont été arrêtés et l'environnement est réinitialisé."  
