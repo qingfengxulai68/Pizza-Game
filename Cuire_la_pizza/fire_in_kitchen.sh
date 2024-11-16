bead6db71b63992a8c8e80d9fb31180375d4dd4e
qingfengxulai68 <166528560+qingfengxulai68@users.noreply.github.com>
2024-11-16 20:16:30 +0100

    Create fire_in_kitchen.sh

---
+++ Cuire_la_pizza/fire_in_kitchen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Simulation d'un incendie dans la cuisine  
+# Ce script simule un processus de feu qui doit être arrêté par le joueur.
+
+while true; do
+    echo " La cuisine est en feu ! Aidez à éteindre l'incendie ! "
+    sleep 5
+done
