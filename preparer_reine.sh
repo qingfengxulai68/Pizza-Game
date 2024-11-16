862d01d7f133fa765d60d2af25d7914ea859d437
giahg294 <giahoang294@gmail.com>
2024-11-16 19:24:00 +0100

    recette

--- preparer_reine.sh
+++ preparer_reine.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Nom du fichier contenant la recette
-fichier_recette="recette.txt"
-
 # Message de démarrage
 echo "=== Préparation de la pizza Reine ==="
 echo "La préparation d'une Reine a commencé. 🍕"
 
 # Créer le fichier avec la recette complète
-echo "=== Recette de la pizza Reine ===" > "$fichier_recette"
-echo "1. Trouver les ingrédients nécessaires dans le dossier ingrédient." >> "$fichier_recette"
-echo "2. Préparer les ingrédients dans le dossier préparation." >> "$fichier_recette"
-echo "3. Cuire la pizza dans le dossier cuisson." >> "$fichier_recette"
-echo "4. Enfin servez la pizza à Yafei et Gia." >> "$fichier_recette"
+echo "=== Recette de la pizza Reine ===" > recette.txt
+echo "1. Trouver les ingrédients nécessaires dans le dossier ingrédient." >> recette.txt
+echo "2. Préparer les ingrédients dans le dossier préparation." >> recette.txt
+echo "3. Cuire la pizza dans le dossier cuisson." >> recette.txt
+echo "4. Enfin servez la pizza à Yafei et Gia." >> recette.txt
 echo "Affichez le fichier '$fichier_recette' pour savoir ce qu'il faut faire."
 
 # Lancer un chronomètre de 1 heure
