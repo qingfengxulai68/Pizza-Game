55fa2bd49c48a4cc6c3a34471e23977c1c49ab87
giahg294 <giahoang294@gmail.com>
2024-11-16 19:33:55 +0100

    creation des dossiers etape

--- preparer_margherita.sh
+++ preparer_margherita.sh
@@ -6,10 +6,10 @@ echo "La préparation d'une Margherita a commencé. 🍕"
 
 # Créer le fichier avec la recette complète
 echo "=== Recette de la pizza Margherita ===" > recette.txt
-echo "1. Trouver les ingrédients nécessaires dans le dossier ingrédient." >> recette.txt
-echo "2. Préparer les ingrédients dans le dossier préparation." >> recette.txt
-echo "3. Cuire la pizza dans le dossier cuisson." >> recette.txt
-echo "4. Enfin servez la pizza à Yafei et Gia." >> recette.txt
+echo "etape 1. Trouver les ingrédients nécessaires dans le dossier ingrédient." >> recette.txt
+echo "etape 2. Préparer les ingrédients dans le dossier préparation." >> recette.txt
+echo "etape 3. Cuire la pizza dans le dossier cuisson." >> recette.txt
+echo "etape 4. Enfin servez la pizza à Yafei et Gia." >> recette.txt
 echo "Affichez le fichier recette.txt pour savoir ce qu'il faut faire."
 
 # Lancer un chronomètre de 1 heure
