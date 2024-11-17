23d2df9f10e2843c1bc0a89f6422586714836413
qingfengxulai68 <166528560+qingfengxulai68@users.noreply.github.com>
2024-11-17 11:21:55 +0100

    Update fire_in_kitchen.sh

--- Cuire_la_pizza/fire_in_kitchen.sh
+++ Cuire_la_pizza/fire_in_kitchen.sh
@@ -4,13 +4,5 @@
 # Simulation d'un incendie dans la cuisine
 # Ce script simule un processus de feu qui doit être arrêté par le joueur.
 
-# Nom du processus pour identification
-fire_process_name="fire_in_kitchen"
+bash -c "sleep 1000" &
 
-"""
-# Boucle infinie pour simuler l'incendie
-while true; do
-    echo "🔥 La cuisine est en feu ! 🔥 - Processus : $fire_process_name"
-    sleep 5
-done
-"""
