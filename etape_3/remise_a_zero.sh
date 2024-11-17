857d733c464da2cea214d33a9b6dc9bd803a6590
giahg294 <giahoang294@gmail.com>
2024-11-17 23:42:04 +0100

    etape 3

--- etape_3/remise_a_zero.sh
+++ etape_3/remise_a_zero.sh
@@ -1,4 +1,13 @@
-$file = 'etat.txt'
+#!/bin/bash
+
+# Nom du fichier
+file="etat.txt"
+
+# Vérification de l'existence du fichier
 if [ -f "$file" ]; then
+    # Suppression du fichier
     rm "$file"
-    echo "Fichier '$file' supprimé."
+    echo "Le fichier '$file' a été supprimé avec succès."
+else
+    echo "Le fichier '$file' n'existe pas."
+fi
