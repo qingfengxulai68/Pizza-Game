a0017ca40f2b4622e83502ac697f8b1b4cef4f12
qingfengxulai68 <166528560+qingfengxulai68@users.noreply.github.com>
2024-11-17 10:02:03 +0100

    Create remise_a_zero.sh

---
+++ Servir_la_pizza/remise_a_zero.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Réinitialisation du niveau 5
+
+# Supprime le répertoire table s'il existe
+if [ -d table ]; then
+    rm -r table
+    echo "Le répertoire 'table' a été supprimé."
+else
+    echo "Le répertoire 'table' n'existait pas."
+fi
+
+# Supprime le fichier pizza.txt s'il existe
+if [ -f pizza.txt ]; then
+    rm pizza.txt
+    echo "Le fichier 'pizza.txt' a été supprimé."
+else
+    echo "Le fichier 'pizza.txt' n'existait pas."
+fi
+
+echo "Le niveau 5 a été réinitialisé. Vous pouvez recommencer."
