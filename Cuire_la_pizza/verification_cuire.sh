ed252da365eebec02928e51d0e32f040d89fc029
qingfengxulai68 <166528560+qingfengxulai68@users.noreply.github.com>
2024-11-17 11:01:18 +0100

    Update verification_cuire.sh

--- Cuire_la_pizza/verification_cuire.sh
+++ Cuire_la_pizza/verification_cuire.sh
@@ -1,9 +1,9 @@
+
+
 #!/bin/bash
 
-# Script pour vérifier si le joueur a correctement terminé le niveau 4
-
 # Vérifie si le processus 'fire_in_kitchen' est toujours actif
-if ps aux | grep -v grep | grep "sh ./.fire_in_kitchen.sh" > /dev/null; then
+if ps aux | grep -v grep | grep "./fire_in_kitchen.sh" > /dev/null; then
     echo "Échec : Le processus 'fire_in_kitchen' est toujours actif. Veuillez le tuer avant de continuer."
     exit 1
 else
